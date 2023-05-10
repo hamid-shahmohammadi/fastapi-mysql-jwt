e6adaf9caceacea0a6f4d2d0c32f73f6abdc921f
Hamid Shah Mohammadi <43700170+hamid-shahmohammadi@users.noreply.github.com>
2023-05-10 15:37:04 +0430

    02-02-20

--- database.py
+++ database.py
@@ -1,9 +1,9 @@
-import sqlalchemy as sqlalchemy
+import sqlalchemy as _sqlalchemy
 import sqlalchemy.ext.declarative as declarative
 import sqlalchemy.orm as orm
 
 DB_URL = "mysql+pymysql://root:@localhost/fastapi_jwt?charset=utf8mb4"
-engine = sqlalchemy.create_engine(DB_URL)
+engine = _sqlalchemy.create_engine(DB_URL,echo=True)
 
 SessionLocal = orm.sessionmaker(autocommit=False,autoflush=False,bind=engine)
 
--- models.py
+++ models.py
@@ -1,15 +1,33 @@
 import datetime as datetime
-import sqlalchemy as sqlalchemy
-import sqlalchemy.orm as orm
+import sqlalchemy as _sqlalchemy
+from sqlalchemy import Column, Date, Integer, String
+import sqlalchemy.orm as _orm
 
-import passlib.hash as hash
-import database as database
+import passlib.hash as _hash
+import database as _database
 
-class UserModel(database.Base):
-    __table__="users"
-    id=sqlalchemy.Column(sqlalchemy.Integer,primary_key=True,index=True)
-    email=sqlalchemy.Column(sqlalchemy.String,unique=True,index=True)
-    name=sqlalchemy.Column(sqlalchemy.String)
-    phone=sqlalchemy.Column(sqlalchemy.String)
-    password_hash=sqlalchemy.Column(sqlalchemy.String)
-    created_at=sqlalchemy.Column(sqlalchemy.DateTime,default=datetime.datetime.utcnow())
+class UserModel(_database.Base):
+    __tablename__= "users"
+    id=_sqlalchemy.Column(_sqlalchemy.Integer,primary_key=True,index=True)
+    email=_sqlalchemy.Column(String(64),unique=True,index=True)
+    name=_sqlalchemy.Column(String(64))
+    phone=_sqlalchemy.Column(String(64))
+    password_hash=_sqlalchemy.Column(String(64))
+    created_at=_sqlalchemy.Column(_sqlalchemy.DateTime,default=datetime.datetime.utcnow())
+    posts = _orm.relationship("Post",back_populates="user")
+
+    def password_verification(self,password:str):
+        return _hash.bcrypt.verify(password, self.password_hash)
+
+
+
+class UserModel(_database.Base):
+    __tablename__="posts"   
+    id=_sqlalchemy.Column(_sqlalchemy.Integer,primary_key=True,index=True) 
+    user_id=_sqlalchemy.Column(_sqlalchemy.Integer,_sqlalchemy.ForeignKey("users.id"))
+    post_title =  _sqlalchemy.Column(String(64),index=True)
+    post_description =  _sqlalchemy.Column(String(64),index=True)
+    image =  _sqlalchemy.Column(String(64))
+    created_at=_sqlalchemy.Column(_sqlalchemy.DateTime,default=datetime.datetime.utcnow())
+    user = _orm.relationship("User",back_populates="posts")
+
